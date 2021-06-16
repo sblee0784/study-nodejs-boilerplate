927233d37318e0754e12bd7079ad97dfac551301
sblee <sblee@synergyai.co>
2021-06-16 17:39:32 +0900

    lecture 12 generate token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require('./models/User');
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //  application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -52,16 +53,21 @@ app.post('/login', (req, resp) => {
         //  요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
         user.comparePassword(req.body.password, (err, isMatch) => {
             if(!isMatch)
-                return res.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+                return resp.json({loginSuccess: false, message: "비밀번호가 틀렸습니다."})
 
             //  비밀번호 까지 맞다면 토큰을 생성하기.
             user.generateToken((err, user) => {
+                if (err)    return resp.status(400).send(err);
 
+                //  토큰을 저장한다. 어디에 ?     쿠키, 로컬스토리지
+                resp.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
             })
         })
     })
 })
 
-ap.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
 })
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
@@ -57,6 +58,24 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
     bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
         if (err)    return cb(err);
         cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function (cb) {
+
+    var user = this;
+
+    //  jsonwebtoken을 이용해서 token을 생성하기
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    // user._id + 'secretToken' = token
+    // ->
+    // 'secretToken' -> user._id
+
+    user.token = token
+    user.save(function(err, user) {
+        if (err)    return cb(err);
+        cb(null, user);
     })
 }
 
