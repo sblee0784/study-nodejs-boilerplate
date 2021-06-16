8370197fd7d4faa2bf8df029325a2e171d04eb05
sblee <sblee@synergyai.co>
2021-06-16 11:42:01 +0900

    lecture 07 register

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+
+//  application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//  application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://johnahn:abcd1234@boilerplate.sogfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -10,6 +18,21 @@ mongoose.connect('mongodb+srv://johnahn:abcd1234@boilerplate.sogfg.mongodb.net/m
 
 app.get('/', (req, resp) => {
     resp.send("Hello World!~~ 안녕하세요 ~ ")
+})
+
+app.post('/register', (req, resp) => {
+
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return resp.json({ success: false, err });
+        return resp.status(200).json({
+            success: true
+        })
+    });
 })
 
 app.listen(port, () => {
